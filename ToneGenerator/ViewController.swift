40a59cc62d00761b40de3b459972f0c5dc421c45
Karina Katrich <katrich@dnt-lab.com>
2016-11-30 00:16:18 +0200

    Initial Commit

---
+++ ToneGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToneGenerator
+//
+//  Created by Karina on 30.11.16.
+//  Copyright © 2016 Karina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
